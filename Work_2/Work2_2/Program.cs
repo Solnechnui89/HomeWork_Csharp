d5837b4fa2a086854c28c749a33a56b88d1b7df4
Ann <pgav89@mail.ru>
2022-06-27 02:18:22 +1000

    work2_2

---
+++ Work_2/Work2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 2;
+int numberB = 10;
+
+if(numberA > numberB)
+{
+    Console.Write("max: ");
+    Console.WriteLine(numberA);
+}
+
+else
+{
+    Console.Write("max: ");
+    Console.WriteLine(numberB);
+}
