16f3e7dc333f7751dd73f0f8c89d73a6a24ae0ac
Ann <pgav89@mail.ru>
2022-06-27 02:28:52 +1000

    Work4_2

---
+++ Work_4/Work4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 44;
+int numberB = 5;
+int numberC = 78;
+
+int max = numberA;
+
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.Write("max: ");
+Console.WriteLine(max);
